e65503c644a33a640b433316ce1ca18cbd08659c
jonathon <jonathonarbaugh@gmail.com>
2018-10-17 00:55:32 -0700

    added explicitness back

--- cards.go
+++ cards.go
@@ -20,21 +20,21 @@ const (
 
 // Kinds
 const (
-	Two Kind = 2
-	Three
-	Four
-	Five
-	Six
-	Seven
-	Eight
-	Nine
-	Ten
-	Jack
-	Queen
-	King
-	Ace
-	LittleJoker
-	BigJoker
+	Two         Kind = 2
+	Three       Kind = 3
+	Four        Kind = 4
+	Five        Kind = 5
+	Six         Kind = 6
+	Seven       Kind = 7
+	Eight       Kind = 8
+	Nine        Kind = 9
+	Ten         Kind = 10
+	Jack        Kind = 11
+	Queen       Kind = 12
+	King        Kind = 13
+	Ace         Kind = 14
+	LittleJoker Kind = 15
+	BigJoker    Kind = 16
 )
 
 var suitNames = map[Suit]string{
